edc9115425d380ee6a032bf5604f9a973b77fadb
ahmad7725 <94556345+ahmad7725@users.noreply.github.com>
2021-11-18 15:29:43 -0600

    udpated stva

--- state variable/sketch.js
+++ state variable/sketch.js
@@ -18,7 +18,7 @@ function draw() {
   line(width/2, 0, width/2,height);
   line(0,height/2,width,height/2);
   updateCurrentSide();
-  print("Left: " + tLeft + "\tRight: " + tRight + "Left: " + bLeft + "\tRight: " + bRight);
+  print(" top Left: " + tLeft + "\t top Right: " + tRight + " bottom Left: " + bLeft + "\t bottom Right: " + bRight);
   //renderRectangles();
 }
 
@@ -26,16 +26,47 @@ function draw() {
 function updateCurrentSide(){
   //update the state variables to represent which side of the canvas
   //the mouse is presently on.
-  if(mouseX < height/2){
-    //mouse is on the LEFT
+  if(mouseX < height/2 && mouseY < width/2){
+    //mouse is on the TOP LEFT
+
     tLeft = true;
     tRight = false;
+    bLeft = false;
+    bRight = false;
+    rect(0,0,width/2,height/2)
+    fill(0);
   }
-  else{
-    //mouse is on the RIGHT
+  else if(mouseX > height/2 && mouseY < width/2){
+    // mouse is on the Top RIGHT
+    rect(width/2,0,width/2,height/2)
     tLeft = false;
     tRight = true;
+    bLeft = false;
+    bRight = false;
+    fill(0);
   }
+  else if ( mouseX < height/2 && mouseY > width/2){ 
+    // mouse is on the bottom left
+    rect(0,height/2,width/2,height/2)
+    bLeft = true;
+    bRight = false;
+    tLeft = false;
+    tRight = false;
+    fill(0);
+  
+  }     
+
+  else{
+    //mouse is on the  Bottom RIGHT
+    rect(width/2,height/2,width/2,height/2)
+    tLeft = false;
+    tRight = false;
+    bRight = true;
+    bLeft = false;
+    fill(0);
+  }
+}
+
 }
 
 
